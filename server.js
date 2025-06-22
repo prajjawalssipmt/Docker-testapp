fafa0a8c04109e787967ca14b89b810c0d9507b2
prajjawalssipmt <tanmayprajjawalgupta@gmail.com>
2025-06-23 01:18:43 +0530

    Update Port id to 5050

--- server.js
+++ server.js
@@ -12,31 +12,30 @@ const client = new MongoClient(MONGO_URL);
 
 //GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+  await client.connect(URL);
+  console.log("Connected successfully to server");
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
-    res.send(data);
+  const db = client.db("apnacollege-db");
+  const data = await db.collection("users").find({}).toArray();
+
+  client.close();
+  res.send(data);
 });
 
 //POST new user
 app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+  const userObj = req.body;
+  console.log(req.body);
+  await client.connect(URL);
+  console.log("Connected successfully to server");
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
+  const db = client.db("apnacollege-db");
+  const data = await db.collection("users").insertOne(userObj);
+  console.log(data);
+  console.log("data inserted in DB");
+  client.close();
 });
 
-
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
